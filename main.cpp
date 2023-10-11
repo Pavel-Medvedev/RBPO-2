6a32e54129a906d2259ca607a800cd18f8f32091
Pavel-Medvedev <medvedev.pavel.al@outlook.com>
2023-10-11 11:54:55 -0700

    Улучшена читаемость кода, добавлены некоторые пояснениие к запрашиваемой у пользователя информации.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 
 import Medvedev1bib21050;
+enum Choice {
+	Task1 = '1', Task2 = '2', Task3 = '3', Task4 = '4', Task5 = '5'
+};
 
 int main() {
 	char q;
 	do
 	{
+		std::cout << "Decide what the program should to do:\
+					\nTo start first task input:\t\'1\'\
+					\nTo start second task input:\t\'2\'\
+					\nTo start third task input:\t\'3\'\
+					\nTo strat fourth task input:\t\'4\'\
+					\nTo start fifth task input:\t\'5\'\
+					\nTo end the session input:\t\'Q\'" << std::endl;
 		std::cin >> q;
-		switch (q)
-		{
+		switch ((Choice)q) {
 			int n; double x, eps;
-		case '1':
+		case Task1:
 			std::cout << "You choose 1 Task to test." << std::endl
 					  << "Input x, n and epsilon:" << std::endl;
 			std::cin >> x >> n >> eps;
@@ -19,7 +28,7 @@ int main() {
 					  << "f3(n) = " << RBPO::Lab2::Variant15::Task1::f3(n) << std::endl
 					  << "f4(eps) = " << RBPO::Lab2::Variant15::Task1::f4(eps) << std::endl;
 			break;
-		case '2':
+		case Task2:
 			std::cout << "You choose 2 Task to test." << std::endl
 				<< "Input x, n and epsilon:" << std::endl;
 			std::cin >> x >> n >> eps;
@@ -28,7 +37,7 @@ int main() {
 				<< "f3(n) = " << RBPO::Lab2::Variant15::Task2::f3(n) << std::endl
 				<< "f4(eps) = " << RBPO::Lab2::Variant15::Task2::f4(eps) << std::endl;
 			break;
-		case '3':
+		case Task3:
 			std::cout << "You choose 3 Task to test." << std::endl
 				<< "Input x, n and epsilon:" << std::endl;
 			std::cin >> x >> n >> eps;
@@ -37,7 +46,7 @@ int main() {
 				<< "f3(n) = " << RBPO::Lab2::Variant15::Task3::f3(n) << std::endl
 				<< "f4(eps) = " << RBPO::Lab2::Variant15::Task3::f4(eps) << std::endl;
 			break;
-		case '4':
+		case Task4:
 			std::cout << "You choose 4 Task to test." << std::endl
 				<< "Input x, n and epsilon:" << std::endl;
 			std::cin >> x >> n >> eps;
@@ -46,7 +55,7 @@ int main() {
 				<< "f3(n) = " << RBPO::Lab2::Variant15::Task4::f3(n) << std::endl
 				<< "f4(eps) = " << RBPO::Lab2::Variant15::Task4::f4(eps) << std::endl;
 			break;
-		case '5':
+		case Task5:
 			std::cout << "You choose 5 Task to test." << std::endl
 				<< "Input x, n and epsilon:" << std::endl;
 			std::cin >> x >> n >> eps;
@@ -58,7 +67,8 @@ int main() {
 		case 'Q':
 			break;
 		default:
-			std::cout << "Inputed data is incorrect!";
+			std::cout << "Inputed data is incorrect!" << std::endl;
+			break;
 		}
 	} while (q != 'Q');
 
@@ -66,5 +76,6 @@ int main() {
 	return 0;
 }
 /*
+* Data for test
 * 1 12.34 100 0.0001 2 12.34 100 0.0001 3 12.34 100 0.0001 4 12.34 100 0.0001 5 12.34 100 0.0001 Q
 */
