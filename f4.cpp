49c4780289db834fe9bb5abfc24bfffe52f257f1
Pavel-Medvedev <medvedev.pavel.al@outlook.com>
2023-10-15 10:45:15 -0700

    Небольшие фиксы.

--- f4.cpp
+++ f4.cpp
@@ -14,7 +14,7 @@ namespace RBPO {
 };
 
 double RBPO::Lab2::Variant15::Task3::f4(double eps) {
-	double sum = a(0), temp = a(1), temp1 = a(2); unsigned long long i = 2;
+	double sum = 0, temp = a(0), temp1 = a(1); unsigned long long i = 1;
 	do {
 		sum += temp, temp = temp1, temp1 = a(++i);
 	} while (eps < std::abs(temp - temp1));
